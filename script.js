f3e528bc4ce561bd5b062b7ff7a3c3a99ccc183f
il-moostro <elmonstromail@gmail.com>
2023-03-17 23:25:24 +0100

    Fix responsive menu

---
+++ script.js
@@ -0,0 +1,5 @@
+const menuIcon=document.querySelector('.menu-icon')
+const menu=document.querySelector('.header-menu')
+menuIcon.addEventListener('click',()=>{
+    menu.classList.toggle('hide-menu')
+})
