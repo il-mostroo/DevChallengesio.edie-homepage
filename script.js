5bc7c7647e035bd86ec6096a5539e03e03a5e9ce
il-moostro <elmonstromail@gmail.com>
2023-03-26 20:17:11 +0000

    Delete animations code and use AOS library instead

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
+
 //mobile navigation menu functionality
+
 const menuIcon=document.querySelector('.menu-icon')
 const menu=document.querySelector('.header-menu')
 menuIcon.addEventListener('click',()=>{
     menu.classList.toggle('hide-menu')
 })
-//add css animations:
-const hiddenSectionsRight=document.querySelectorAll('.hidden-right')
-const observerRight=new IntersectionObserver((entries)=>{
-    entries.forEach((entry)=>{
-        console.log(entry)
-        if(entry.isIntersecting){
-            entry.target.classList.add("show")
-        }
-        else{
-            entry.target.classList.remove("show")
-        }
-    })
-})
-hiddenSectionsRight.forEach(section => observerRight.observe(section));
 
-const hiddenSectionsLeft=document.querySelectorAll('.hidden-left')
-const observerLeft=new IntersectionObserver((entries)=>{
-    entries.forEach((entry)=>{
-        console.log(entry)
-        if(entry.isIntersecting){
-            entry.target.classList.add("show")
-        }
-        else{
-            entry.target.classList.remove("show")
-        }
-    })
-})
-hiddenSectionsLeft.forEach(section => observerLeft.observe(section));
+
